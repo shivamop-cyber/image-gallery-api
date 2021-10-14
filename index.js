91dab80cb0f96bf1c19c96fb7736071720fb6a94
Shivam Singh <shivamsingh00141@gmail.com>
2021-10-14 19:21:25 +0530

    added cors

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
+const cors = require('cors');
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 8800;
 const ImageSchema = new mongoose.Schema(
   {
